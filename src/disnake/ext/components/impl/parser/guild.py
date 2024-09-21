2cec3ef17c3571a53eefb570287fcb217235e794
Chromosomologist <chromosomologist@gmail.com>
2024-09-21 13:52:29 +0200

    refactor!: make guild parsers use internal int parser

--- src/disnake/ext/components/impl/parser/guild.py
+++ src/disnake/ext/components/impl/parser/guild.py
@@ -6,7 +6,8 @@ import typing
 
 import disnake
 from disnake.ext.components.impl.parser import base as parser_base
-from disnake.ext.components.impl.parser import helpers, snowflake
+from disnake.ext.components.impl.parser import builtins as builtins_parsers
+from disnake.ext.components.impl.parser import helpers
 
 __all__: typing.Sequence[str] = (
     "GuildParser",
@@ -17,13 +18,36 @@ __all__: typing.Sequence[str] = (
 )
 
 
+def _get_guild_from_source(
+    source: object,
+) -> disnake.Guild:
+    if isinstance(source, helpers.GuildAware) and source.guild:
+        return source.guild
+
+    if isinstance(source, helpers.MessageAware):
+        return _get_guild_from_source(source.message)
+
+    if isinstance(source, helpers.ChannelAware):
+        return _get_guild_from_source(source.channel)
+
+    msg = "Cannot get a role from an object that doesn't reference a guild."
+    raise TypeError(msg)
+
+
 @parser_base.register_parser_for(disnake.Guild)
 class GetGuildParser(parser_base.SourcedParser[disnake.Guild]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    int_parser: builtins_parsers.IntParser
+
+    def __init__(
+        self,
+        int_parser: typing.Optional[builtins_parsers.IntParser] = None,
+        *,
+        allow_fallback: bool = False,
+    ):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
+        self.allow_fallback = allow_fallback
 
     def loads(  # noqa: D102
         self,
@@ -33,30 +57,46 @@ class GetGuildParser(parser_base.SourcedParser[disnake.Guild]):  # noqa: D101
     ) -> disnake.Guild:
         # <<docstring inherited from parser_api.Parser>>
 
+        guild_id = self.int_parser.loads(argument)
+
         if isinstance(source, helpers.BotAware):
-            guild = source.bot.get_guild(int(argument))
+            guild = source.bot.get_guild(guild_id)
+            if guild:
+                return guild
 
-            if (
-                guild is None
-                and isinstance(source, helpers.GuildAware)
-                and source.guild
-            ):
-                return source.guild
-
-        elif source.guild:
+        # If allow_fallback is True, return the guild regardless of
+        # whether the id is correct. Otherwise, validate the id.
+        if (
+            isinstance(source, helpers.GuildAware)
+            and source.guild
+            and (self.allow_fallback or source.guild.id == guild_id)
+        ):
             return source.guild
 
         msg = f"Could not find a guild with id {argument!r}."
         raise LookupError(msg)
+
+    def dumps(self, argument: disnake.Guild) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
 
 
 @parser_base.register_parser_for(disnake.Guild)
 class GuildParser(parser_base.SourcedParser[disnake.Guild]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    int_parser: builtins_parsers.IntParser
+    allow_fallback: bool
+
+    def __init__(
+        self,
+        int_parser: typing.Optional[builtins_parsers.IntParser] = None,
+        *,
+        allow_fallback: bool = False,
+    ):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
+        self.allow_fallback = allow_fallback
 
     async def loads(  # noqa: D102
         self,
@@ -66,23 +106,33 @@ class GuildParser(parser_base.SourcedParser[disnake.Guild]):  # noqa: D101
     ) -> disnake.Guild:
         # <<docstring inherited from parser_api.Parser>>
 
-        id_ = int(argument)
+        guild_id = self.int_parser.loads(argument)
         if isinstance(source, helpers.BotAware):
-            guild = source.bot.get_guild(id_)
+            guild = source.bot.get_guild(guild_id)
             if guild:
                 return guild
 
             try:
-                return await source.bot.fetch_guild(id_)
+                return await source.bot.fetch_guild(guild_id)
             except disnake.HTTPException:
-                if isinstance(source, helpers.GuildAware) and source.guild:
-                    return source.guild
+                pass
 
-        elif source.guild:
+        # If allow_fallback is True, return the source guild regardless of
+        # whether the id is correct. Otherwise, validate the id.
+        if (
+            isinstance(source, helpers.GuildAware)
+            and source.guild
+            and (self.allow_fallback or source.guild.id == guild_id)
+        ):
             return source.guild
 
         msg = f"Could not find a guild with id {argument!r}."
         raise LookupError(msg)
+
+    def dumps(self, argument: disnake.Guild) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
 
 
 @parser_base.register_parser_for(disnake.Invite)
@@ -122,9 +172,10 @@ class InviteParser(parser_base.SourcedParser[disnake.Invite]):  # noqa: D101
 class GetRoleParser(parser_base.SourcedParser[disnake.Role]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    int_parser: builtins_parsers.IntParser
+
+    def __init__(self, int_parser: typing.Optional[builtins_parsers.IntParser] = None):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
 
     def loads(  # noqa: D102
         self,
@@ -138,39 +189,30 @@ class GetRoleParser(parser_base.SourcedParser[disnake.Role]):  # noqa: D101
     ) -> disnake.Role:
         # <<docstring inherited from parser_api.Parser>>
 
-        guild = None
-        if isinstance(source, helpers.GuildAware):
-            guild = source.guild
+        guild = _get_guild_from_source(source)
+        role_id = self.int_parser.loads(argument)
 
-        if guild is None and isinstance(source, helpers.MessageAware):
-            guild = source.message.guild
-
-        if (
-            guild is None
-            and isinstance(source, helpers.ChannelAware)
-            and isinstance(source.channel, helpers.GuildAware)
-        ):
-            guild = source.channel.guild
-
-        if guild is None:
-            msg = "Cannot get a role from an object that doesn't reference a guild."
-            raise TypeError(msg)
-
-        role = guild.get_role(int(argument))
-        if role is not None:
+        role = guild.get_role(role_id)
+        if role:
             return role
 
         msg = f"Could not find a role with id {argument!r}."
         raise LookupError(msg)
+
+    def dumps(self, argument: disnake.Role) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
 
 
 @parser_base.register_parser_for(disnake.Role)
 class RoleParser(parser_base.SourcedParser[disnake.Role]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    int_parser: builtins_parsers.IntParser
+
+    def __init__(self, int_parser: typing.Optional[builtins_parsers.IntParser] = None):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
 
     async def loads(  # noqa: D102
         self,
@@ -184,37 +226,18 @@ class RoleParser(parser_base.SourcedParser[disnake.Role]):  # noqa: D101
     ) -> disnake.Role:
         # <<docstring inherited from parser_api.Parser>>
 
-        guild = None
-        if isinstance(source, helpers.GuildAware):
-            guild = source.guild
+        guild = _get_guild_from_source(source)
+        role_id = self.int_parser.loads(argument)
 
-        if guild is None and isinstance(source, helpers.MessageAware):
-            guild = source.message.guild
-
-        if (
-            guild is None
-            and isinstance(source, helpers.ChannelAware)
-            and isinstance(source.channel, helpers.GuildAware)
-        ):
-            guild = source.channel.guild
-
-        if guild is None:
-            msg = "Cannot get a role from an object that doesn't reference a guild."
-            raise TypeError(msg)
-
-        id_ = int(argument)
-        role = guild.get_role(id_)
+        role = guild.get_role(role_id)
         if role is not None:
             return role
 
         for role in await guild.fetch_roles():
-            if role.id == id_:
+            if role.id == role_id:
                 return role
 
-        # a role id coming from a custom_id could be of a deleted role object
-        # so we're handling that possibility
-        if role is None:
-            msg = f"Could not find a role with id {argument!r}."
-            raise LookupError(msg)
-
-        return role
+        # A role id coming from a custom_id could be of a deleted role object
+        # so we're handling that possibility.
+        msg = f"Could not find a role with id {argument!r}."
+        raise LookupError(msg)
