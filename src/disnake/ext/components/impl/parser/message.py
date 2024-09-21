c4c8f44debc58cb63dc89f7c9dbae3475b945345
Chromosomologist <chromosomologist@gmail.com>
2024-09-21 14:21:38 +0200

    refactor!: make message parsers use internal int parser

--- src/disnake/ext/components/impl/parser/message.py
+++ src/disnake/ext/components/impl/parser/message.py
@@ -6,7 +6,8 @@ import typing
 
 import disnake
 from disnake.ext.components.impl.parser import base as parser_base
-from disnake.ext.components.impl.parser import helpers, snowflake
+from disnake.ext.components.impl.parser import builtins as builtins_parsers
+from disnake.ext.components.impl.parser import helpers
 
 __all__: typing.Sequence[str] = (
     "GetMessageParser",
@@ -27,9 +28,17 @@ AnyChannel = typing.Union[
 class GetMessageParser(parser_base.SourcedParser[disnake.Message]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    int_parser: builtins_parsers.IntParser
+    allow_fallback: bool
+
+    def __init__(
+        self,
+        int_parser: typing.Optional[builtins_parsers.IntParser] = None,
+        *,
+        allow_fallback: bool = False,
+    ):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
+        self.allow_fallback = allow_fallback
 
     def loads(  # noqa: D102
         self,
@@ -37,55 +46,115 @@ class GetMessageParser(parser_base.SourcedParser[disnake.Message]):  # noqa: D10
         *,
         source: typing.Union[helpers.BotAware, helpers.MessageAware],
     ) -> disnake.Message:
+        message_id = self.int_parser.loads(argument)
+
         if isinstance(source, helpers.BotAware):
-            message = source.bot.get_message(int(argument))
+            message = source.bot.get_message(message_id)
             if message:
                 return message
 
-            if isinstance(source, helpers.MessageAware):
-                return source.message
-
-        else:
+        # If allow_fallback is True, return the source message regardless of
+        # whether the id is correct. Otherwise, validate the id.
+        if (
+            isinstance(source, helpers.MessageAware)
+            and (self.allow_fallback or source.message.id == message_id)
+        ):  # fmt: skip
             return source.message
 
         msg = f"Could not find a message with id {argument!r}."
         raise LookupError(msg)
+
+    def dumps(self, argument: disnake.Message) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
 
 
 @parser_base.register_parser_for(disnake.Message)
 class MessageParser(parser_base.SourcedParser[disnake.Message]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.dumps = snowflake.snowflake_dumps
+    def __init__(
+        self,
+        int_parser: typing.Optional[builtins_parsers.IntParser] = None,
+        *,
+        allow_fallback: bool = False,
+    ):
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
+        self.allow_fallback = allow_fallback
 
     async def loads(  # noqa: D102
-        self, argument: str, *, source: helpers.ChannelAware
+        self,
+        argument: str,
+        *,
+        source: typing.Union[
+            helpers.BotAware,
+            helpers.ChannelAware,
+            helpers.MessageAware,
+        ],
     ) -> disnake.Message:
         # <<docstring inherited from parser_api.Parser>>
 
+        message_id = self.int_parser.loads(argument)
         if isinstance(source, helpers.BotAware):
-            message = source.bot.get_message(int(argument))
+            message = source.bot.get_message(message_id)
             if message:
                 return message
 
-        return await source.channel.fetch_message(int(argument))
+        if isinstance(source, helpers.ChannelAware):
+            message = await source.channel.fetch_message(message_id)
+            if message:
+                return message
+
+        # If allow_fallback is True, return the source message regardless of
+        # whether the id is correct. Otherwise, validate the id.
+        if (
+            isinstance(source, helpers.MessageAware)
+            and (self.allow_fallback or source.message.id == message_id)
+        ):  # fmt: skip
+            return source.message
+
+        msg = f"Could not find a message with id {argument!r}."
+        raise LookupError(msg)
+
+    def dumps(self, argument: disnake.Message) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
 
 
 @parser_base.register_parser_for(disnake.PartialMessage)
 class PartialMessageParser(parser_base.SourcedParser[disnake.PartialMessage]):  # noqa: D101
     # <<docstring inherited from parser_api.Parser>>
 
-    def __init__(self, channel: typing.Optional[AnyChannel] = None) -> None:
+    def __init__(
+        self,
+        int_parser: typing.Optional[builtins_parsers.IntParser] = None,
+        *,
+        channel: typing.Optional[AnyChannel] = None,
+    ) -> None:
+        self.int_parser = int_parser or builtins_parsers.IntParser.default()
         self.channel = channel
-        self.dumps = snowflake.snowflake_dumps
 
     def loads(  # noqa: D102
-        self, argument: str, *, source: helpers.ChannelAware
+        self, argument: str, *, source: object
     ) -> disnake.PartialMessage:
         # <<docstring inherited from parser_api.Parser>>
 
-        return disnake.PartialMessage(
-            channel=self.channel or source.channel, id=int(argument)
-        )
+        channel = self.channel
+        if not channel and isinstance(source, helpers.ChannelAware):
+            channel = source.channel
+
+        if not channel:
+            msg = (
+                "A channel must be provided either through self.channel or"
+                " source.channel, got neither."
+            )
+            raise RuntimeError(msg)
+
+        return channel.get_partial_message(self.int_parser.loads(argument))
+
+    def dumps(self, argument: disnake.PartialMessage) -> str:  # noqa: D102
+        # <<docstring inherited from parser_api.Parser>>
+
+        return self.int_parser.dumps(argument.id)
