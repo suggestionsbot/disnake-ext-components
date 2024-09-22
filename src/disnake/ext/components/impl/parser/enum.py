e34b8f2a370824b0991a8615007c044530fd17c6
Chromosomologist <chromosomologist@gmail.com>
2024-09-22 17:57:29 +0200

    refactor!: let enum parsers parse by name too

--- src/disnake/ext/components/impl/parser/enum.py
+++ src/disnake/ext/components/impl/parser/enum.py
@@ -13,7 +13,7 @@ _AnyEnum = typing.Union[enum.Enum, disnake.Enum, disnake.flags.BaseFlags]
 _EnumT = typing.TypeVar("_EnumT", bound=_AnyEnum)
 
 
-def _get_enum_type(enum_class: typing.Type[_AnyEnum]) -> type:
+def _get_enum_type(enum_class: typing.Type[_AnyEnum]) -> typing.Optional[type]:
     if issubclass(enum_class, disnake.flags.BaseFlags):
         return int
 
@@ -31,9 +31,8 @@ def _get_enum_type(enum_class: typing.Type[_AnyEnum]) -> type:
     if all(type(member.value) == maybe_type for member in member_iter):
         return maybe_type
 
-    # No concrete type, throw hands.
-    msg = "Cannot parse enums with more than one value type."
-    raise TypeError(msg)
+    # No single type; store by name instead.
+    return None
 
 
 @parser_base.register_parser_for(
@@ -59,20 +58,71 @@ class EnumParser(parser_base.SourcedParser[_EnumT]):
     enum_class: typing.Type[_EnumT]
     value_parser: parser_base.AnyParser
 
-    def __init__(self, enum_class: typing.Type[_EnumT]) -> None:
+    def __init__(
+        self,
+        enum_class: typing.Type[_EnumT],
+        *,
+        store_by_value: typing.Optional[bool] = None,
+    ) -> None:
+        value_type = _get_enum_type(enum_class)
+
+        if issubclass(enum_class, disnake.flags.BaseFlags) and store_by_value is False:
+            msg = (
+                "Cannot store disnake flags by name, as their members do not have"
+                " names."
+            )
+            raise ValueError(msg)
+
+        # If the enum is not of a single type and store_by_value wasn't
+        # explicitly set or was explicitly set to false, parse by name.
+        if value_type is None and not store_by_value:
+            self.store_by_value = False
+            self.value_type = str
+
+        # If the value type could be determined, set it on the parser and
+        # respect the input for store_by_value-- default to True for integers.
+        elif value_type is not None:
+            self.store_by_value = (
+                issubclass(value_type, int)
+                if store_by_value is None
+                else store_by_value
+            )  # fmt: skip
+            self.value_type = value_type
+
+        # If the value type could not be determined but the user explicitly set
+        # store_by_value to True, raise an exception.
+        else:
+            msg = (
+                "Cannot store enum members by value for enums with members of"
+                " varying types. Please either set `store_by_value` to False"
+                " or make sure all enum members are of the same type."
+            )
+            raise ValueError(msg)
+
         self.enum_class = enum_class
-        self.value_parser = parser_base.get_parser(_get_enum_type(enum_class))
+        self.value_parser = parser_base.get_parser(self.value_type)
 
     async def loads(self, argument: str, *, source: object) -> _EnumT:  # noqa: D102
         # <<docstring inherited from parser_api.Parser>>
 
         parsed = await parser_base.try_loads(self.value_parser, argument, source=source)
-        return self.enum_class(parsed)  # pyright: ignore[reportCallIssue]
+
+        if self.store_by_value:
+            return self.enum_class(parsed)  # pyright: ignore[reportCallIssue]
+        else:
+            return self.enum_class[parsed]  # pyright: ignore[reportInvalidTypeArguments]
 
     async def dumps(self, argument: _EnumT) -> str:  # noqa: D102
         # <<docstring inherited from parser_api.Parser>>
+        if self.store_by_value:
+            result = self.value_parser.dumps(argument.value)
+        else:
+            # Baseflags members are always integers. This should never error
+            # due to the check in __init__.
+            assert not isinstance(argument, disnake.flags.BaseFlags)
+            result = self.value_parser.dumps(argument.name)
 
-        return await aio.eval_maybe_coro(self.value_parser.dumps(argument.value))
+        return await aio.eval_maybe_coro(result)
 
 
 FlagParser = EnumParser
